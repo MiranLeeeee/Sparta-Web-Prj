8d777873fbe3772f73e93019979b44a534031090
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-01-03 20:44:17 +0900

    update app.py (subpages)

--- project/app.py
+++ project/app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, jsonify, request
-import requests
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
+
+@app.route('/deposit')
+def deposit():
+    return render_template('deposit.html')
+
+@app.route('/savings')
+def savings():
+    return render_template('savings.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
