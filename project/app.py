8c8b509e0315003407df595b68c57bc67c08d46c
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-01-21 13:34:58 +0900

    update app.py (getBankNames)

--- project/app.py
+++ project/app.py
@@ -57,5 +57,11 @@ def validUserPW():
 
     return jsonify({'result': 'success', 'countResult': result})
 
+#은행 정보 가져오기
+@app.route('/getBankNames', methods=['GET'])
+def getBankNames():
+    result = list(db.banks.find({}, {'_id': 0}))
+    return jsonify({'result': 'success', 'banks': result})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
