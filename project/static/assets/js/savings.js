d824e148c1dec3ccc15ba60afb4237f4cc613318
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-02-19 22:58:59 +0900

    update savings.js (getProducts)

--- project/static/assets/js/savings.js
+++ project/static/assets/js/savings.js
@@ -49,3 +49,23 @@ function getBankNames(){
         }//success end
     })//ajax end
 }
+
+//은행상품정보 데이터가져오기
+function getProducts() {
+    let jsonResult;
+    let baseList = [];
+	let optionList = [];
+
+    $.ajax({
+        type: "GET",
+        url: "/getSavingsData",
+        data: {},
+        async: false,
+        success: function(response){
+            jsonResult = JSON.parse(response);
+            baseList = jsonResult["result"]["baseList"];
+            optionList = jsonResult["result"]["optionList"];
+        }
+    })
+    return [baseList, optionList]
+}
