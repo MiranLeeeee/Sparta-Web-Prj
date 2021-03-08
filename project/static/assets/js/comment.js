b1d7e9260be01bbb801b7e7cc9a41496ea36e9e5
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-03-08 15:17:15 +0900

    update comment.js (session)

--- project/static/assets/js/comment.js
+++ project/static/assets/js/comment.js
@@ -1,10 +1,16 @@
 let session_name = ""
+let prd_nm = ""
 
 $(document).ready(function(){
     let url = String(decodeURI (location.href))
-    let url_list= new Array()
+    let url_list = new Array()
     let url_list = url.split("&&")
 
     let session_id = (url_list[1].split("="))[1]
+    session_name = getName(session_id)
+    prd_nm = (url_list[0].split("="))[1]
 
+    $('#prd_nm').empty()
+    $('#prd_nm').append(prd_nm)
 })
+
