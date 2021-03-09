1a14d1a4666d770055f12c6529141069ac0bc182
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-03-09 17:51:09 +0900

    update app.py (getName)

--- project/app.py
+++ project/app.py
@@ -105,5 +105,13 @@ def updateLike():
 def comment():
     return render_template('comment.html')
 
+#세션이름 가져오기
+@app.route('/getName', methods=['POST'])
+def getName():
+    sessionId = request.form['session_id']
+
+    result = db.member.find_one({'id': sessionId}, {'_id': 0})
+    return jsonify({'result': result})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
